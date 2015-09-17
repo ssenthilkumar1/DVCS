47bf3ced49698a5ee362ff1cb0a65bb8fdc2a4cb
Sudha Senthilkumar <ssenthilkumar1@studentmail.nait.ca>
2015-09-16 20:27:29 -0600

    DVCS Save

---
+++ DVCS_WebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVCS_WebSite.Startup))]
+namespace DVCS_WebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
